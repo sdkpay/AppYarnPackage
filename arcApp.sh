fb23bc6e0947f88b252e28b89cc99caf415a0387
AlexanderIpatov <65159241+AlexIpatov@users.noreply.github.com>
2023-06-21 09:18:44 +0300

    fir for python scripts

--- arcApp.sh
+++ arcApp.sh
@@ -1,12 +1,37 @@
 #!/bin/bash
 
-xcodebuild \
--project SPaySdk.xcodeproj \
--scheme SPaySdkExample \
--archivePath ./buildApp/Archive/SPaySdkExample.xcarchive archive
+PRINT_COUNT=1
+ROW_STRING="==========================================="
 
-xcodebuild \
--exportArchive \
--archivePath ./buildApp/Archive/SPaySdkExample.xcarchive \
--exportPath ./buildApp/ipa \
--exportOptionsPlist ./BuildOptions/ExportOptions.plist
+printStep() {
+    echo "${ROW_STRING}"
+    echo "$PRINT_COUNT." $1
+    echo "${ROW_STRING}"
+    let "PRINT_COUNT+=1"
+}
+
+clear() {
+    printStep "Clear build folder"
+    rm -r ./buildApp/*
+}
+
+buildArchive() {
+    printStep "Build Archive"
+    xcodebuild \
+    -project SPaySdk.xcodeproj \
+    -scheme SPaySdkExample \
+    -archivePath ./buildApp/Archive/SPaySdkExample.xcarchive archive
+}
+
+makeIPA() {
+    printStep "Make IPA"
+    xcodebuild \
+    -exportArchive \
+    -archivePath ./buildApp/Archive/SPaySdkExample.xcarchive \
+    -exportPath ./buildApp/ipa \
+    -exportOptionsPlist ./BuildOptions/ExportOptions.plist
+}
+
+clear
+buildArchive
+makeIPA
--- build.py
+++ build.py
@@ -19,15 +19,15 @@ def main(command):
  """
 Утилита для работы с SPay SDK\n
 Список команд:\n
- 1. makeSDK - Собирает артефакт, готовый к передаче мерчу\n
- 2. makeProject - Собирает файл project и генерирует ресурсы\n
- 3. archiveTest - Собирает .ipa файл тестового приложения с сдк
+ 1. make_sdk - Собирает артефакт, готовый к передаче мерчу\n
+ 2. make_project - Собирает файл project и генерирует ресурсы\n
+ 3. make_ipa - Собирает .ipa файл тестового приложения с сдк
  """
- if command == "makeSDK":
+ if command == "make_sdk":
   runBuildSDK()
- elif command == "makeProject":
+ elif command == "make_project":
   buildProject()
- elif command == "archiveTest":
+ elif command == "make_ipa":
   archiveTest()
  else:
   print("❌ Неверная команда, запусти build.py --help")
