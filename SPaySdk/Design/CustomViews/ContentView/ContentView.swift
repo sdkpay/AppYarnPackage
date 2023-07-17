6789110cd97eaa2560366a45ed5d519c19df65ce
AlexanderIpatov <65159241+AlexIpatov@users.noreply.github.com>
2023-07-17 19:56:39 +0300

    Add cms cert 1.06

--- SPaySdk/Design/CustomViews/ContentView/ContentView.swift
+++ SPaySdk/Design/CustomViews/ContentView/ContentView.swift
@@ -25,8 +25,7 @@ class ContentView: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    
+
     @objc
     private func tapped() {
         action?()
--- SPaySdk/Services/NetworkService/CertificateValidator/Certificates.swift
+++ SPaySdk/Services/NetworkService/CertificateValidator/Certificates.swift
@@ -9,6 +9,7 @@ import Foundation
 
 enum CertificatesKeys: String, CaseIterable {
     case subCA = "c5vrLWsonReRbgbtO/d8quvwiejpedqth2k3GA3xB2U="
+    case cms = "8Zfp1LAJpCyXmDDrT6O7fcNWRIIJnRcUExv17jA50Wc="
 
     static var allValues: [String] {
         self.allCases.map { $0.rawValue }
