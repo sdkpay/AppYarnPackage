6941c757ff77e2af246c2cb722c292079e49ec65
AlexanderIpatov <65159241+AlexIpatov@users.noreply.github.com>
2023-09-13 16:15:06 +0300

    Рефакторинг auth метода

--- SPaySdk/Extensions/String+sha256.swift
+++ SPaySdk/Extensions/String+sha256.swift
@@ -12,7 +12,7 @@ extension Data {
         return hexStringFromData(input: digest(input: self as NSData))
     }
     
-    private func digest(input : NSData) -> NSData {
+    private func digest(input: NSData) -> NSData {
         let digestLength = Int(CC_SHA256_DIGEST_LENGTH)
         var hash = [UInt8](repeating: 0, count: digestLength)
         CC_SHA256(input.bytes, UInt32(input.length), &hash)
@@ -25,7 +25,7 @@ extension Data {
         
         var hexString = ""
         for byte in bytes {
-            hexString += String(format:"%02x", UInt8(byte))
+            hexString += String(format: "%02x", UInt8(byte))
         }
         
         return hexString
