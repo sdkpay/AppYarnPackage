3dec1ed6882989d7add49cd7c2462d06d85b0704
AlexanderIpatov <65159241+AlexIpatov@users.noreply.github.com>
2023-09-20 15:22:16 +0300

    Release to Testflight. Version: 1.2 (33)

--- SPaySdk/Services/AuthService/AuthService.swift
+++ SPaySdk/Services/AuthService/AuthService.swift
@@ -165,7 +165,8 @@ final class DefaultAuthService: AuthService, ResponseDecoder {
                     self.analytics.sendEvent(event)
                 }
                 
-                if refreshIsActive && self.featureToggleService.isEnabled(.refresh) {
+//                if refreshIsActive && self.featureToggleService.isEnabled(.refresh) {
+                if refreshIsActive {
                     self.authManager.authMethod = .refresh
                     self.authСompletion?(nil, false)
                     self.authСompletion = nil
