ed48e56e3de39393dcf46cd63c4270db1bb5adf8
AlexanderIpatov <65159241+AlexIpatov@users.noreply.github.com>
2023-11-08 17:58:10 +0300

    Откат на хардкор в кнопке BNPL

--- SPaySdk/Presentation/PaymentVC/PaymentFeaturesConfig.swift
+++ SPaySdk/Presentation/PaymentVC/PaymentFeaturesConfig.swift
@@ -30,7 +30,7 @@ enum PaymentFeaturesConfig {
     static func configPartModel(partPayService: PartPayService) -> PaymentCellModel {
         guard let buttonBnpl = partPayService.bnplplan?.buttonBnpl else { return PaymentCellModel() }
         let icon = partPayService.bnplplanSelected ? buttonBnpl.activeButtonLogo : buttonBnpl.inactiveButtonLogo
-        let subtitle = buttonBnpl.content ?? ""
+        let subtitle = (partPayService.bnplplanSelected ? buttonBnpl.content : Strings.Part.Inactive.title) ?? ""
         return PaymentCellModel(title: buttonBnpl.header ?? "",
                                 subtitle: subtitle,
                                 iconURL: icon,
