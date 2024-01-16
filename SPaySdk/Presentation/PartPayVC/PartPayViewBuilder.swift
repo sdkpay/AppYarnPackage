eab51af1b016f924065ba3de1a3d9af963d1bc2a
AlexanderIpatov <65159241+AlexIpatov@users.noreply.github.com>
2024-01-16 15:35:09 +0300

    Исправил зону нажатия на ссылку в bnpl + верная текстовка для кнопки бнпл

--- SPaySdk/Presentation/PartPayVC/PartPayViewBuilder.swift
+++ SPaySdk/Presentation/PartPayVC/PartPayViewBuilder.swift
@@ -45,7 +45,7 @@ private extension PartPayViewBuilder {
             }
             
             enum Cancel {
-                static let title = String(stringLiteral: Strings.Cancel.title)
+                static let title = String(stringLiteral: Strings.Part.Pay.Cancel.title)
                 
                 static let topOffSet: CGFloat = 20.0
                 static let leftOffSet: CGFloat = Consts.margin
