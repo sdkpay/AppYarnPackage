a6c4c7f611564ae807d7414504b2e7a2a99bef06
AlexanderIpatov <65159241+AlexIpatov@users.noreply.github.com>
2024-01-23 02:23:57 +0300

    Правка работы таймера в бэкграунде

--- SPaySdk/Managers/TimerManager/TimerManager.swift
+++ SPaySdk/Managers/TimerManager/TimerManager.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 typealias IntAction = ((Int) -> Void)
 
@@ -23,9 +24,38 @@ final class DefaultTimerManager: TimerManager {
     
     private var maxSec: Int = 0
     private var currentSec: Int = 0
+    private var appDidEnterBackgroundDate: Date?
     
     func setup(sec: Int) {
         self.maxSec = sec
+        setupNotifications()
+    }
+    
+    private func setupNotifications() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(applicationDidEnterBackground(_:)),
+                                               name: UIApplication.didEnterBackgroundNotification, 
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(applicationWillEnterForeground(_:)),
+                                               name: UIApplication.willEnterForegroundNotification, object: nil)
+    }
+    
+    private func removeNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIApplication.didEnterBackgroundNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIApplication.willEnterForegroundNotification, object: nil)
+    }
+    
+    @objc func applicationDidEnterBackground(_ notification: NotificationCenter) {
+        appDidEnterBackgroundDate = Date()
+    }
+
+    @objc func applicationWillEnterForeground(_ notification: NotificationCenter) {
+        guard let previousDate = appDidEnterBackgroundDate else { return }
+        let calendar = Calendar.current
+        let difference = calendar.dateComponents([.second], from: previousDate, to: Date())
+        let seconds = difference.second!
+        currentSec -= seconds
     }
     
     func start() {
@@ -50,6 +80,7 @@ final class DefaultTimerManager: TimerManager {
         
         timer?.invalidate()
         timer = nil
+        removeNotifications()
     }
     
     @objc
