c925d557958f2e3ad55d1be0f0ef6517ca6a7bde
AlexanderIpatov <65159241+AlexIpatov@users.noreply.github.com>
2024-03-27 10:53:44 +0300

    Поправил метрику

--- SPaySdk/Presentation/PaymentVC/Modules/PaymentPartPayModuleVC/PaymentPartPayModulePresenter.swift
+++ SPaySdk/Presentation/PaymentVC/Modules/PaymentPartPayModuleVC/PaymentPartPayModulePresenter.swift
@@ -79,7 +79,7 @@ final class PaymentPartPayModulePresenter: NSObject, PaymentPartPayModulePresent
         
         analytics.send(EventBuilder()
             .with(base: .Touch)
-            .with(value: MetricsValue(rawValue: "Value"))
+            .with(value: MetricsValue(rawValue: "Pay"))
             .build(), on: view?.contentParrent?.analyticsName ?? .None)
         
         Task {
